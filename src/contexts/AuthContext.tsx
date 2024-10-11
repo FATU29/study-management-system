a9b14a6b49a0753f72ea0eb0b91ced3ac0f475d8
FAT29 <lqm550@gmail.com>
2024-10-11 09:48:35 +0700

    22120264-SecondChange-Template

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,53 @@
+import { TUser } from "src/types/userType"
+import { AuthValuesType, LoginParams } from "./types"
+import { createContext, ReactNode, useState } from "react"
+
+
+
+type Props = {
+    children: ReactNode
+}
+
+const defaultProvider: AuthValuesType = {
+    user: null,
+    loading: true,
+    setUser: () => null,
+    setLoading: () => Boolean,
+    login: () => Promise.resolve(),
+    logout: () => Promise.resolve()
+  }
+
+const AuthContex = createContext(defaultProvider);
+
+
+
+const AuthProvider = ({children} : Props) => {
+    
+    const [user,setUser] = useState<TUser | null>(defaultProvider.user);
+    const [loading,setLoading] = useState<boolean>(defaultProvider.loading);
+
+
+    const handleLogin = async (params: LoginParams) => {
+        
+    }
+
+    const handleLogout = async () => {
+
+    }
+
+
+    const values : AuthValuesType = {
+        user: user,
+        setUser: setUser,
+        loading: loading,
+        setLoading: setLoading,
+        login: handleLogin,
+        logout:handleLogout
+    }
+
+
+    return <AuthContex.Provider value={values}>{children}</AuthContex.Provider>
+}
+
+
+export default AuthProvider;
---
+++ src/contexts/types.ts
@@ -0,0 +1,19 @@
+import { TUser } from "src/types/userType"
+
+export type LoginParams = {
+    email: string
+    password: string
+    rememberMe?: boolean
+  }
+
+
+
+
+export type AuthValuesType = {
+    loading: boolean
+    logout: () => void
+    user: TUser | null
+    setLoading: (value: boolean) => void
+    setUser: (value: TUser | null) => void
+    login: (params: LoginParams) => void
+  }
