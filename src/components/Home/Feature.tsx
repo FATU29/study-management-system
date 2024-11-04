786a03579b98e9408e61610339a37107063c554a
FAT29 <lqm550@gmail.com>
2024-11-04 12:35:34 +0700

    Fix conflict

--- src/components/Home/Feature.tsx
+++ src/components/Home/Feature.tsx
@@ -1,7 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import IconifyIcon from '../utils/icon';
-
 const Feature = () => {
   return (
     <div className="container">
