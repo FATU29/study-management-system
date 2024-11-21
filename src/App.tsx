5c559d3ba1363191a42e60a48ce1ff7fbe05e631
FAT29 <lqm550@gmail.com>
2024-11-21 22:56:29 +0700

    Change Update

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const AppContent: React.FC = () => {
         >
           <div className="page">
             <Routes location={location}>
-              <Route index path="/" element={<HomePage />} />
+              <Route index path="/" element={<ModalPage />} />
               <Route path="/home" element={<DashboardPage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
